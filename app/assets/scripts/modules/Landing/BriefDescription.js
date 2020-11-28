71cdf8423f988fd7b3360bb92b418978c433c893
mistyjack <oladepomustafa@gmail.com>
2020-11-28 10:02:45 +0100

    Completed brief description .

--- app/assets/scripts/modules/Landing/BriefDescription.js
+++ app/assets/scripts/modules/Landing/BriefDescription.js
@@ -1,12 +1,15 @@
 import React, { useEffect } from "react"
+import alexMouth from "../../../images/alexmouth.jpg"
 
 function BriefDescription() {
   return (
     <div className="page-section container">
-      <div className="row">
-        <div className="col-md"></div>
+      <div className="row px-3">
         <div className="col-md">
-          <h3 className="display-4 text-storm">Alexmouth is a renowned film director, influencer and movie maker popularly known for his strong reputation in the industry.</h3>
+          <img className="img-fluid" sizes="637px" src={alexMouth} alt="Alexmouth" />
+        </div>
+        <div className="col-md align-self-center pl-lg-5 mt-3 mt-md-0">
+          <h3 className="display-4 text-storm">Alexmouth is a renowned film director, an influencer and a movie maker popularly known for his strong reputation in the industry.</h3>
         </div>
       </div>
     </div>
