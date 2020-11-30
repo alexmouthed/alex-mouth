f761111a73a394ba08c8d431dec0e24ee199f8ca
mistyjack <oladepomustafa@gmail.com>
2020-11-30 20:12:51 +0100

    Added more image fixes.

--- app/assets/scripts/modules/Landing/WorkItem.js
+++ app/assets/scripts/modules/Landing/WorkItem.js
@@ -5,9 +5,9 @@ function WorkItem(props) {
   if (props.left) {
     return (
       <Fade left>
-        <div className="work-item py-3 rounded px-4 my-3">
+        <div className="work-item rounded px-4 my-3">
           <a className="text--no-decoration" href={props.item.link}>
-            {props.item.image && <img src={props.item.image} alt="" className="image-fluid" />}
+            {props.item.image && <img className="work-item__image" src={props.item.image} alt="" className="image-fluid" />}
             <h2 className="work-item__title">{props.item.title}</h2>
             <h3 className="work-item__role">Role: {props.item.role}</h3>
             <span className="work-item__date">Released {props.item.date}</span>
@@ -20,7 +20,7 @@ function WorkItem(props) {
       <Fade right>
         <div className="work-item py-3 rounded px-4 my-3">
           <a className="text--no-decoration" href={props.item.link}>
-            {props.item.image && <img src={props.item.image} alt="" className="image-fluid" />}
+            {props.item.image && <img className="work-item__image" src={props.item.image} alt="" className="image-fluid" />}
             <h2 className="work-item__title">{props.item.title}</h2>
             <h3 className="work-item__role">Role: {props.item.role}</h3>
             <span className="work-item__date">Released {props.item.date}</span>
