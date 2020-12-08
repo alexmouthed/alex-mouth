340cb4a6b6bc3c7c81fb620cc1180d5c428a2590
mistyjack <oladepomustafa@gmail.com>
2020-12-08 17:38:40 +0100

    Lazy loaded images.

--- app/assets/scripts/modules/Landing/WorkItem.js
+++ app/assets/scripts/modules/Landing/WorkItem.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react"
 import Fade from "react-reveal/Fade"
+import { LazyLoadImage } from "react-lazy-load-image-component"
 
 function WorkItem(props) {
   if (props.left) {
@@ -7,7 +8,7 @@ function WorkItem(props) {
       <Fade left>
         <div className="work-item rounded px-4 my-3">
           <a className="text--no-decoration" href={`/video-reel#${props.item.title}`}>
-            {props.item.image && <img className="work-item__image" src={props.item.image} alt="" className="image-fluid" />}
+            {props.item.image && <LazyLoadImage className="work-item__image" src={props.item.image} alt={props.item.title} className="image-fluid" />}
             <h2 className="work-item__title">{props.item.title}</h2>
             <h3 className="work-item__role">Role: {props.item.role}</h3>
             <span className="work-item__date">Released {props.item.date}</span>
@@ -20,7 +21,7 @@ function WorkItem(props) {
       <Fade right>
         <div className="work-item py-3 rounded px-4 my-3">
           <a className="text--no-decoration" href={`/video-reel#${props.item.title}`}>
-            {props.item.image && <img className="work-item__image" src={props.item.image} alt="" className="image-fluid" />}
+            {props.item.image && <LazyLoadImage className="work-item__image" src={props.item.image} alt={props.item.title} className="image-fluid" />}
             <h2 className="work-item__title">{props.item.title}</h2>
             <h3 className="work-item__role">Role: {props.item.role}</h3>
             <span className="work-item__date">Released {props.item.date}</span>
