48b14c2b2d28c76bffe91ef3eae0834b08b3d6eb
mistyjack <oladepomustafa@gmail.com>
2020-12-10 16:58:28 +0100

    Edits.

--- app/assets/scripts/modules/MnetData.js
+++ app/assets/scripts/modules/MnetData.js
@@ -24,12 +24,6 @@ const Data = [
     information: "Anita, she's Intelligent, brilliant, hard working but something vital is missing."
   },
   {
-    title: "Silence",
-    role: "Director",
-    date: "November 2013",
-    information: "A stage in Onipede’s family, a family of fame and affluence, with an only child,Gift. Gift hates her parents dearly and as such, hates whatever comes from them. She got married to the son of her father’s best friend and expectedly the hatred transcends to her husband"
-  },
-  {
     title: "Complicated",
     role: "Director",
     date: "October 2013",
@@ -78,59 +72,10 @@ const Data = [
     information: "Different from the norm, there stories intertwined. A story of Love gone sour,molestation, deception and taking a stand between good and bad"
   },
   {
-    title: "The Target",
-    role: "Director",
-    date: "2013 - November 2014",
-    information: "A banker who sleep with older ladies to meet up to his target finds life difficult when bank started having financial set backs and all the staff started getting laid off including him."
-  },
-  {
-    title: "Somewhere Down The Line",
-    role: "Director",
-    date: "2014 - March 2014 (3 months)",
-    information: "Surrogacy becomes a difficult knot in this enthralling movie about infertility and the bond between a mother and her child."
-  },
-  {
     title: "wine For Chocolate",
     role: "Director",
     date: "February 2014",
     information: "An ambitious young chef sets out to dupe his kind and unsuspecting boss. With the help of his girlfriend, the fraud is hatched and executed except for one thing…conscience creeps in."
-  },
-  {
-    title: "Murder at Prime Suites",
-    role: "Actor",
-    date: "January 2013 - September 2013",
-    information: "Murder at Prime Suites is an investigative thriller produced by Jumafor Ajogwu and directed by Chris Eneng"
-  },
-  {
-    title: "Strive",
-    role: "Director, Cinematographer",
-    date: "2012 - 2013",
-    information: "Two young men (Frank and Ray –Yemi Blaq and Bryan Okwara) and their struggle for survival in the rough environment in which they find themselves where drug deals and armed robbery were the order of the day. Things begin to change as Ray decides to take another path and change his ways while Frank ventures into working for BJAY – the most notorious King pin ever known played by Bimbo Manuel."
-  },
-  {
-    title: "Covert Operations",
-    role: "Director, Cinematographer, Editor",
-    date: "2011 - 2013",
-    information: "There was a general plan by Senator Ivan (Olu Jacobs) and his cabal of power hungry men to subvert the Nation and eventually take over the leadership."
-  },
-  {
-    title: "I Voted Now Wetin",
-    role: "Director, Cinematograper, Editor",
-    date: "2012",
-    information: "'I Voted, Now Wetin' film encourages and sparks the culture of post election engagement with elected officials among young people in Nigeria. "
-  },
-  {
-    title: "The Revelation",
-    role: "Director, Cinematograper, Editor",
-    date: "2012",
-    information: "Directed by Alex Mouth from an original screenplay by Kunle Dada, The Revelation tells the story of TBWA/TORONTO's rise from mediocrity to greatness in true Nollywood style. "
-  },
-  {
-    title: "My Vote Fit Change Naija",
-    role: "Director, Cinematograper, Editor",
-    date: "2012",
-    information: "My Vote Fit Change Naija project', a 15 minute voter education short film starring comedian Julius Agwu, Nollywood actress Adaora Ukoh and musician Page 10 of 12 2shot was presented to the public on Thursday, 3rd March 2011 at the Rockview Hotel in Abuja",
-    last: true
   }
 ]
 
